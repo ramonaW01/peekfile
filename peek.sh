5ef042c0e3c5ca6008b91063328113dc672886a0
ramonaW01 <ramonamaria.walch01@estudiant.upf.edu>
2025-11-04 18:48:54 +0100

    Implement line count warning in peek.sh
    Add line count check before displaying file content

--- peek.sh
+++ peek.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 n=$2
+lines=$(wc -l < "$1")
 
-if [[ -z "$n" ]]; then 
-n=3; fi 
+if (( lines < 2 * n )); then cat $1; else echo "warning! File has $lines lines!"; fi
+
+if [[ -z "$n" ]]; then n=3; fi 
 
 head -n"$n" $1
 echo ... 
