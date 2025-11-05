421c51b4c937eace5a4a1730597de88b4e5ea996
ramonaW01 <ramonamaria.walch01@estudiant.upf.edu>
2025-11-05 19:57:53 +0100

    updated homework

--- countlines.sh
+++ countlines.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
+for i in "$@"; do
+	if [ -f "$i" ]; then echo "$i";
+	n=$(wc -l < "$i");
+		if [ "$n" -eq 0 ]; then echo "This file has zero lines.";
+		elif [ "$n" -eq 1 ]; then echo "This file has one line.";
+		else echo "This file has $n lines.";
+		fi
+	fi
+done
 
-i=list_of_tables.txt
-n=$(wc -l < $i )
-
-echo $i $n
 
 
-if [ "$n" -eq 0 ]; then
-    echo "This file has zero lines."
-elif [ "$n" -eq 1 ]; then
-    echo "This file has one line."
-else echo "This file has $n lines."
-fi
